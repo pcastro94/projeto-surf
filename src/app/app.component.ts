9569bdd991e657a45df9731d4abe96a8fbec35ed
Pedro Castro <pedrocastro94@gmail.com>
2018-01-29 11:18:01 -0200

    ultimasAlteracoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  escondeMiolo = false;
+  esconder() {
+    this.escondeMiolo = true;
+    console.log('oi');
+  }
 }
